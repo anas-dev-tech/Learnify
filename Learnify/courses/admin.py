ae2d5663cc72a88eed4b786067f9010b87af5b66
Anas Al-Ward <anasalwardtech@gmail.com>
2024-09-12 17:33:18 +0300

    initial commit

---
+++ Learnify/courses/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Subject, Course, Module
+
+
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    '''Admin View for Course'''
+
+    list_display = ('title', 'subject', 'created')
+    list_filter = ('created', 'subject')
+    inlines = [
+        'title' , 
+        'overview'
+    ]
+    search_fields = ('title', 'overview')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
